7b1713550cd44d221e7e0e980d130f85fa07bb4b
RadWEB2000 <radoslaw.adamczyk2000@gmail.com>
2026-07-19 20:24:45 +0200

    update fonts

--- assets/fonts/index.ts
+++ assets/fonts/index.ts
@@ -1,17 +1,18 @@
-import {Geologica, Geist_Mono, PT_Serif, Inter_Tight} from "next/font/google";
+import {Geologica, Fira_Mono, PT_Serif, Inter_Tight, Encode_Sans, Merriweather} from "next/font/google";
 
-const  fontSerif = PT_Serif({
+const  fontSerif = Merriweather({
     subsets:['latin-ext'],
     weight:['400','700'],
     variable:'--font-serif'
 })
 
-const fontData = Geist_Mono({
+const fontData = Fira_Mono({
     subsets:['latin-ext'],
+    weight:['400','700'],
     variable:'--font-data'
 })
 
-const fontSans = Inter_Tight({
+const fontSans = Encode_Sans({
     subsets:['latin-ext'],
     variable:'--font-sans'
 })
--- components/views/home-view/Hero.tsx
+++ components/views/home-view/Hero.tsx
@@ -1,6 +1,6 @@
 export default function Hero(){
     return (
-        <header className="bg-primary">
+        <header className="bg-brand min-h-[95vh]">
             asd
         </header>
     )
